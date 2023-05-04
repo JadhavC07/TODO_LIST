db1e405fe11217f759abde5bc1f9aa12c38c2cd8
JadhavC07 <cjaddhav@gmail.com>
2023-05-04 16:46:42 +0530

    "added number state to count todo task"

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -4,22 +4,22 @@ import "./Todo.css";
 const TodoList: any = () => {
   const [task, setTask] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
-
+  const [count, setCount] = useState<number>(0);
 
   const handleInput = (event: any) => {
     setInputValue(event.target.value);
   };
 
-  const inputEmpty = <span> Tast is Empty, write something</span>;
   const handleAddTodo = () => {
     if (inputValue.trim() === "") {
-      return { inputEmpty };
+      return alert("Input in Empty");
     }
     setTask([...task, inputValue]);
     setInputValue("");
+    setCount(count + 1);
   };
 
-  const todoListMap = task.map((todo, index) => <li key={index}>{todo}</li>);
+  const todoListMap = task.map((todo, index) => <li key={index}> {todo}</li>);
 
   return (
     <>
@@ -32,16 +32,19 @@ const TodoList: any = () => {
           value={inputValue}
           onChange={handleInput}
         />
-        <button
-          type="button"
-          className="btn btn-primary my-2 right"
-          onClick={handleAddTodo}
-        >
-          Add TODO
-        </button>
-        <ol>
-          {todoListMap}
-        </ol>
+        <div className="d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-primary my-2 right"
+            onClick={handleAddTodo}
+          >
+            Add TODO
+          </button>
+          <button type="button" className="btn btn-light my-2 border">
+            {count}
+          </button>
+        </div>
+        <ol>{todoListMap}</ol>
       </div>
     </>
   );
